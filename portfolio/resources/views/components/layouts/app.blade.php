b267b8fd2f091da329b74a6b9c401eb27bd5919b
STICKnoLOGIC <65322242+STICKnoLOGIC@users.noreply.github.com>
2026-06-12 20:32:23 +0800

    Update: use a cdnjs of FontAwesome Library instead of self hosted one

--- portfolio/resources/views/components/layouts/app.blade.php
+++ portfolio/resources/views/components/layouts/app.blade.php
@@ -24,6 +24,7 @@
 
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @production
         <script defer src="{{ env('ANALYTICS_URL') }}" data-website-id="{{ env('ANALYTICS_WEBSITE_ID') }}"></script>
     @endproduction
