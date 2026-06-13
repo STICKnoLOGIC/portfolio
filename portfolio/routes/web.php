3d704308dba9a89c7464e345a79068e4b35ab063
STICKnoLOGIC <65322242+STICKnoLOGIC@users.noreply.github.com>
2026-06-13 15:42:44 +0800

    Update: add health checker and fix white spaces

--- portfolio/routes/web.php
+++ portfolio/routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
+use App\Http\Controllers\AsHumanController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\CaseStudyController;
+use App\Http\Controllers\CreativeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\{
-    HomeController,
-    BlogController,
-    CaseStudyController,
-    ProjectController,
-    CreativeController,
-    AboutController,
-    AsHumanController
-};
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -18,7 +14,9 @@ Route::view('/about', 'pages.about')->name('about');
 Route::view('/contact-us', 'pages.contact-us')->name('contact-us');
 Route::view('/write-with-us', 'pages.write-with-us')->name('write-with-us');
 
-Route::get('/health', function () { return response()->json(['status' => 'ok','timestamp' => now()]); });
+Route::get('/up', function () {
+    return response()->json(['status' => 'ok', 'timestamp' => now()]);
+});
 
 Route::get('/as-human', [AsHumanController::class, 'index'])->name('as-human');
 Route::get('/as-human/{slug}', [AsHumanController::class, 'show'])->name('as-human.show');
@@ -32,10 +30,10 @@ Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
 Route::get('/projects/{slug}', [ProjectController::class, 'show'])->name('projects.show');
 Route::get('/projects/tech/{tech}', [ProjectController::class, 'tech'])->name('projects.technologies');
 
-Route::get('/case-studies', [CaseStudyController::class,'index'])->name('case-studies');
-Route::get('/case-studies/{slug}', [CaseStudyController::class,'show'])->name('case-studies.show');
-Route::get('/case-studies/tag/{tag}', [CaseStudyController::class,'tags'])->name('case-studies.tags');
+Route::get('/case-studies', [CaseStudyController::class, 'index'])->name('case-studies');
+Route::get('/case-studies/{slug}', [CaseStudyController::class, 'show'])->name('case-studies.show');
+Route::get('/case-studies/tag/{tag}', [CaseStudyController::class, 'tags'])->name('case-studies.tags');
 
-Route::get('/creative', [CreativeController::class,'index'])->name('creative');
-Route::get('/creative/{slug}', [CreativeController::class,'show'])->name('creative.show');
-Route::get('/creative/tags/{tag}', [CreativeController::class,'tags'])->name('creative.tags');
+Route::get('/creative', [CreativeController::class, 'index'])->name('creative');
+Route::get('/creative/{slug}', [CreativeController::class, 'show'])->name('creative.show');
+Route::get('/creative/tags/{tag}', [CreativeController::class, 'tags'])->name('creative.tags');
