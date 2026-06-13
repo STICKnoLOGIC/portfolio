f178e8625d07c60fb19e89caac8ee1a20dbcba90
STICKnoLOGIC <65322242+STICKnoLOGIC@users.noreply.github.com>
2026-06-13 15:26:34 +0800

    Feat: Custom error page

--- portfolio/bootstrap/app.php
+++ portfolio/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            return response()->view('pages.error', [], 404);
+        });
+
+        // Custom view for all other errors (e.g., 500)
+        $exceptions->renderable(function (Throwable $e, $request) {
+            return response()->view('pages.error', [], 500);
+        });
     })->create();
